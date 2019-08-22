ed6f7a79bc3ce4312629db85ca9b05a27e92cc40
Simeon Ehrig <s.ehrig@hzdr.de>
2019-08-22 10:28:00 +0200

    Add sample project
    - the project contains an executable, a header, and a shared library to simulate a real self-compiled application

---
+++ hello_world_tool/include/hello.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+std::string hello();
+
+std::string world();
---
+++ hello_world_tool/lib/hello.cpp
@@ -0,0 +1,5 @@
+#include "hello.h"
+
+std::string hello() { return std::string("Hello"); }
+
+std::string world() { return std::string("World"); }
