b07af4c371600d0238f4cb8d9eb3a45793b0a53f
Simeon Ehrig <s.ehrig@hzdr.de>
2019-08-27 14:20:53 +0200

    Add openssl build to the single-level build
    - demonstrate the use of a build system other than cmake
    - bump singularity version to 3.3

--- single-stage/recipe.py
+++ single-stage/recipe.py
@@ -4,11 +4,39 @@ import hpccm
 from hpccm.primitives import baseimage, copy, shell, runscript
 from hpccm.building_blocks.cmake import cmake
 from hpccm.templates.CMakeBuild import CMakeBuild
+from hpccm.templates.wget import wget
+from hpccm.templates.tar import tar
 from hpccm.building_blocks.packages import packages
+
+def build_openssl(name: str, build_dir: str) -> [str]:
+    """install openssl
+    :param name: name of the version (e.g. openssl-1.1.1c)
+                 should be sliced from the official URL
+    :type name: str
+    :param build_dir: path where source code is stored and built
+    :type build_dir: str
+    :returns: list of bash commands
+    :rtype: [str]
+    """
+    cm=[]
+    wget_ssl = wget()
+    tar_ssl = tar()
+    cm.append(wget_ssl.download_step(url='https://www.openssl.org/source/'+name+'.tar.gz',
+                                     directory=build_dir))
+    cm.append(tar_ssl.untar_step(tarball=build_dir+'/'+name+'.tar.gz', directory=build_dir))
+    cm.append('cd '+build_dir+'/'+name)
+    cm.append('./config -Wl,-rpath=/usr/local/lib')
+    cm.append('make -j')
+    cm.append('make install -j')
+    cm.append('cd -')
+    return cm
 
 def get_stage(container):
     # generate baseimage
     hpccm.config.set_container_format(container)
+    # version >3.2 is necessary for multi-stage build
+    if container == 'singularity':
+        hpccm.config.set_singularity_version('3.3')
     Stage0 = hpccm.Stage()
     Stage0 += baseimage(image='ubuntu:bionic')
 
@@ -16,12 +44,13 @@ def get_stage(container):
     if container == 'singularity':
         Stage0 += copy(src='../hello_world_tool', dest='/opt/')
     else:
-        # docker
+        # docker: cannot copy files from outsite the build context
+        # so, we need to move the build context one level up
         Stage0 += copy(src='./hello_world_tool', dest='/opt/hello_world_tool')
 
     # install compiler tools
-    Stage0 += cmake(eula=True)
-    Stage0 += packages(ospackages=['g++', 'make'])
+    Stage0 += cmake(eula=True, version='3.14.5')
+    Stage0 += packages(ospackages=['g++', 'make', 'wget', 'build-essential'])
 
     # build and install project
     cmb = CMakeBuild()
@@ -31,6 +60,9 @@ def get_stage(container):
     cm.append(cmb.build_step(target='install'))
     Stage0 += shell(commands=cm)
 
+    Stage0 += shell(commands=build_openssl(name='openssl-1.1.1c',
+                                            build_dir='/opt/openssl_build'))
+
     # script that runs when
     # - singularity uses the run parameter or the image runs directly
     # - docker uses the run parameter without arguments
@@ -39,7 +71,6 @@ def get_stage(container):
     return Stage0
 
 def main():
-
     parser = argparse.ArgumentParser(
         description='Build commands:\n sudo singularity build single-stage.sif singularity.def\n singularity build --fakeroot single-stage.sif singularity.def\n cd .. && docker build -f single-stage/Dockerfile -t single-stage:dev . && cd single-stage\n\nRun commands:\n ./singularity\n docker run single-stage:dev',
         formatter_class=argparse.RawTextHelpFormatter)
@@ -50,7 +81,6 @@ def main():
 
     with open("singularity.def", 'w') as filehandle:
         filehandle.write(get_stage('singularity').__str__())
-
 
 if __name__ == '__main__':
     main()
